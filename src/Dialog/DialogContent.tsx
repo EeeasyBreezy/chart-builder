6f69335c6fd220551418be5bd80c3c7e329835d0
Alex Gavrilov <alex@talentmesh.com>
2024-05-31 22:52:14 +0300

    use options in list

--- src/Dialog/DialogContent.tsx
+++ src/Dialog/DialogContent.tsx
@@ -2,22 +2,15 @@ import { FormikDropdown } from '@/Components/FormikDropDown';
 import FormikTextField from '@/Components/FormikTextField';
 import UIStrings from '@/utils/UIStrings';
 import { Stack, useTheme } from '@mui/material';
+import useChartOptions from './useChartOptions';
 
 export default function DialogBody(): JSX.Element {
     const theme = useTheme();
+    const options = useChartOptions();
 
     return (
         <Stack direction="column" spacing={theme.spacing(2)} paddingY={theme.spacing(2)}>
-            <FormikDropdown
-                label={`${UIStrings.Measurement}*`}
-                name="chartType"
-                options={[
-                    {
-                        id: 'xxx',
-                        name: 'CPI',
-                    },
-                ]}
-            />
+            <FormikDropdown label={`${UIStrings.Measurement}*`} name="chartType" options={options} />
             <FormikTextField label={`${UIStrings.Title}*`} name="title" />
             <FormikTextField label={`${UIStrings.XAxis}*`} name="xAxis" />
             <FormikTextField label={`${UIStrings.YAxis}*`} name="yAxis" />
