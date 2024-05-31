c9c9d40dffdae7ddc038d1a35c7574381e001ce4
Alex Gavrilov <alex@talentmesh.com>
2024-05-31 23:54:20 +0300

    mapping

--- src/app/ChartList/index.tsx
+++ src/app/ChartList/index.tsx
@@ -1,16 +1,18 @@
 import LineChart from '@/Charts/LineChart';
 import { useChartContext } from '@/State/useChartContext';
 import { Stack, useTheme } from '@mui/material';
+import useMapping from './useMapping';
 
 export default function ChartList(): JSX.Element {
     const { charts } = useChartContext();
     const theme = useTheme();
+    const { convertToProps } = useMapping();
 
     return (
         <Stack direction="column" spacing={theme.spacing(2)}>
             {charts.map((chart) => {
                 if (chart.type == 'line') {
-                    return <LineChart key={chart.id} {...chart} />;
+                    return <LineChart key={chart.id} {...convertToProps(chart)} />;
                 }
             })}
         </Stack>
---
+++ src/app/ChartList/useMapping.ts
@@ -0,0 +1,31 @@
+import BaseChartProps from "@/Charts/ChartProps";
+import { LineChartProps } from "@/Charts/LineChart";
+import { Chart } from "@/Models/Chart";
+
+interface ChartMapping {
+    convertToProps: (chart: Chart) => LineChartProps;
+}
+export default function useMapping():ChartMapping {
+
+    const convertToProps = (chart: Chart): LineChartProps => {
+        const result: LineChartProps ={
+            title: chart.title,
+            plotColor: chart.plotColor,
+            xAxisColor: chart.xAxisColor,
+            yAxisColor: chart.yAxisColor,
+            xLabel: chart.xLabel,
+            yLabel: chart.yLabel,
+            hideXLabel: chart.hideXLabel,
+            hideYLabel: chart.hideYLabel,
+            points: chart.points,
+            lineStyle: chart.lineStyle,
+            showPoints: chart.showPoints
+        };
+
+        return result;
+    };
+
+    return {
+        convertToProps,
+    };
+}
