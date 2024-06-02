9371ea48d47f75c2df726f4d7ffdd8f5d7877ef8
Alex Gavrilov <alex@talentmesh.com>
2024-06-02 17:18:58 +0300

    jest config + test coverage

--- __tests__/obsevations.test.ts
+++ __tests__/obsevations.test.ts
@@ -1,9 +1,20 @@
+import SeriesFredClient from '@/Clients/Fred/SeriesFredClient';
 import handler from '../pages/api/observations';
 import { NextApiRequest, NextApiResponse } from 'next';
 import { RequestMethod, createMocks } from 'node-mocks-http';
+import Fred from 'node-fred';
 
 // Mock the SeriesFredClient module
 jest.mock('../src/Clients/Fred/SeriesFredClient');
+
+// Provide a custom implementation for getObservations
+(SeriesFredClient as jest.MockedClass<typeof SeriesFredClient>).mockImplementation(() => {
+    return {
+        client: {} as Fred,
+        getSeries: jest.fn().mockResolvedValue({}),
+        getObservations: jest.fn().mockResolvedValue([]),
+    };
+});
 
 describe('handler', () => {
     function mockArguments(method: RequestMethod, id?: string, frequency?: string, unit?: string, aggregate?: string) {
@@ -18,9 +29,9 @@ describe('handler', () => {
         });
     }
 
-    test.each([
+    it.each([
         ['CONNECT'], ['DELETE'], ['HEAD'], ['OPTIONS'], ['PATCH'], ['POST'], ['PUT'], ['TRACE']
-    ])('returns 405 for %s method', async (method) => {
+    ])('Should return 405 for %s method', async (method) => {
         const { req, res } = mockArguments(method as RequestMethod, 'test', 'test', 'test', 'test');
 
         await handler(req, res);
@@ -30,7 +41,7 @@ describe('handler', () => {
         expect(response.message).toStrictEqual('Method not allowed');
     });
 
-    test('returns 400 for missing id', async () => {
+    it('Should return 400 for missing id', async () => {
         const { req, res } = mockArguments("GET", undefined, 'test', 'test', 'test');
 
         await handler(req, res);
@@ -40,7 +51,7 @@ describe('handler', () => {
         expect(response.message).toStrictEqual("'id' parameter is invalid");
     });
 
-    test('returns 400 when frequency is empty', async () => {
+    test('Should return 400 when frequency is empty', async () => {
         const { req, res } = mockArguments("GET", "test", undefined, 'test', 'test');
 
         await handler(req, res as unknown as NextApiResponse);
@@ -50,7 +61,7 @@ describe('handler', () => {
         expect(response.message).toStrictEqual("'frequency' parameter is invalid");
     });
 
-    test('returns 400 when unit is empty or null', async () => {
+    test('Should return 400 when unit is empty or null', async () => {
         const { req, res } = mockArguments("GET", "test", 'test', undefined, 'test');
 
         await handler(req, res);
@@ -62,13 +73,13 @@ describe('handler', () => {
 
     // Add similar tests for missing frequency and unit
 
-    test('returns 200 and series data for valid request', async () => {
+    test('Should return 200 and series data for valid request', async () => {
         const { req, res } = mockArguments("GET", "test", 'test', 'test', 'test');
 
         await handler(req, res);
         const response = await JSON.parse(res._getData());
 
         expect(res.statusCode).toStrictEqual(200);
-        expect(response).toStrictEqual({ observations: [] });
+        expect(response).toStrictEqual([]);
     });
 });
