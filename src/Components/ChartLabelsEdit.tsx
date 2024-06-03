45e7cd12ac101983671ca69fa4758ef7e85d5688
Alex Gavrilov <alex@talentmesh.com>
2024-06-03 20:36:42 +0300

    data cy

--- src/Components/ChartLabelsEdit.tsx
+++ src/Components/ChartLabelsEdit.tsx
@@ -4,9 +4,9 @@ import FormikTextField from './FormikTextField';
 export default function ChartLabelsEdit(): JSX.Element {
     return (
         <>
-            <FormikTextField label={`${UIStrings.Title}*`} name="title" />
-            <FormikTextField label={`${UIStrings.XAxis}*`} name="xLabel" />
-            <FormikTextField label={`${UIStrings.YAxis}*`} name="yLabel" />
+            <FormikTextField label={`${UIStrings.Title}*`} name="title" data-cy="editTitle" />
+            <FormikTextField label={`${UIStrings.XAxis}*`} name="xLabel" data-cy="editXlabel" />
+            <FormikTextField label={`${UIStrings.YAxis}*`} name="yLabel" data-cy="editYLabel" />
         </>
     );
 }
