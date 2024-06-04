6af0029ad4866188b6aa6ba6c3dff11c5eabc81c
Alex Gavrilov <alex@talentmesh.com>
2024-06-04 04:26:43 +0300

    refactor and clean up

--- src/Components/ColorPicker.tsx
+++ src/Components/ColorPicker.tsx
@@ -1,31 +1,6 @@
-import { makeStyles } from '@mui/styles';
 import React, { useState } from 'react';
 import { ColorResult, SketchPicker } from 'react-color';
 import { Box, Popover, Stack, Typography } from '@mui/material';
-
-const useStyles = makeStyles(() => ({
-    colorBox: {
-        width: '36px',
-        height: '14px',
-        borderRadius: '2px',
-        display: 'inline-block',
-    },
-    swatch: {
-        padding: '5px',
-        background: '#fff',
-        borderRadius: '1px',
-        boxShadow: '0 0 0 1px rgba(0,0,0,.1)',
-        cursor: 'pointer',
-        display: 'inline-block',
-    },
-    cover: {
-        position: 'fixed',
-        top: '0px',
-        right: '0px',
-        bottom: '0px',
-        left: '0px',
-    },
-}));
 
 interface ColorPickerProps {
     label: string;
@@ -35,7 +10,6 @@ interface ColorPickerProps {
 
 const ColorPicker = ({ label, color, onChange }: ColorPickerProps) => {
     const [anchorEl, setAnchorEl] = useState<HTMLDivElement | null>(null);
-    const classes = useStyles();
 
     const handleClick = (event: React.MouseEvent<HTMLDivElement>) => {
         setAnchorEl(anchorEl ? null : event.currentTarget);
@@ -51,8 +25,26 @@ const ColorPicker = ({ label, color, onChange }: ColorPickerProps) => {
     return (
         <Box sx={{ display: 'flex' }}>
             <Stack direction="row" spacing={2} flex={1} alignItems="center">
-                <Box className={classes.swatch} onClick={handleClick}>
-                    <Box className={classes.colorBox} style={{ background: color }} />
+                <Box
+                    onClick={handleClick}
+                    sx={{
+                        padding: '5px',
+                        background: '#fff',
+                        borderRadius: '1px',
+                        boxShadow: '0 0 0 1px rgba(0,0,0,.1)',
+                        cursor: 'pointer',
+                        display: 'inline-block',
+                    }}
+                >
+                    <Box
+                        style={{
+                            background: color,
+                            width: '36px',
+                            height: '14px',
+                            borderRadius: '2px',
+                            display: 'inline-block',
+                        }}
+                    />
                 </Box>
                 <Typography variant="body2">{label}</Typography>
             </Stack>
